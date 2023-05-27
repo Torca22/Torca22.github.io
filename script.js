9b45b630dd92726a0c4711705b81705b53678d18
Torca22 <111666716+Torca22@users.noreply.github.com>
2023-05-26 21:21:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const nombre = document.getElementById("nombreAlumno")
+const apellido = document.getElementById("apellidoAlumno")
+const edad = document.getElementById("edad")
+const dni = document.getElementById("dni")
+const carreras = document.getElementById("carreras_de_grado")
+const email = document.getElementById("email")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/
+    
+    if(nombre.value.length <3){
+       warnings += `- El nombre no es valido <br>`
+       entrar = true
+    }
+    if(apellido.value.length === 0){
+        warnings += `- El apellido no es valido <br>`
+        entrar = true
+     }
+     if(dni.value.length === 8){
+     }
+     else{
+        warnings += `- El DNI no es valido <br>`
+        entrar = true
+     }
+
+    if(!regexEmail.test(email.value)){
+        warnings += `- El email no es valido <br>`
+        entrar = true
+    }
+    if (carreras_de_grado.selectedIndex === 0){
+        warnings += `Seleccione una carrera <br>`
+        entrar = true
+    }
+    if(consulta.value.length <3){
+        warnings += `- No olvide escribir su consulta <br>`
+        entrar = true
+    }
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }
+
+    
+})
